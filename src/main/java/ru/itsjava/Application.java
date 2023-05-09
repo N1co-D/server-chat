2aa339c70d100f6e5aac73e1b7c3a00c6f3f1db2
dmark <dmarkevich@gmail.com>
2023-05-09 04:16:42 +0300

    Chat 5

--- src/main/java/ru/itsjava/Application.java
+++ src/main/java/ru/itsjava/Application.java
@@ -7,5 +7,12 @@ public class Application {
     public static void main(String[] args) {
         ServerService serverService = new ServerServiceImpl();
         serverService.start();
+
+//        Props props = new Props();
+//        System.out.println("props.getValue(\"db.url\") = " + props.getValue("db.url"));
+
+//        UserDao userDao = new UserDaoImpl(new Props());
+//        System.out.println("userDao.findByNameAndPassword(\"Alex\", \"qwerty\") = " + userDao.findByNameAndPassword("Alex", "qwerty"));
+
     }
 }
